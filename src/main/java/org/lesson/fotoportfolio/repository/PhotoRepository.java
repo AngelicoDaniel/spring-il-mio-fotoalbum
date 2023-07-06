e311cee7a3a4b87bd6ca9e97690bfbda08526cb4
Daniel <angelicodaniel93@gmail.com>
2023-07-06 16:39:59 +0200

    push

--- src/main/java/org/lesson/fotoportfolio/repository/PhotoRepository.java
+++ src/main/java/org/lesson/fotoportfolio/repository/PhotoRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
-    List<Photo> findByTitle(String title);
+    // metodo per cercare sul DB in base al titolo della foto
+//    List<Photo> findByTitle(String title);
+    List<Photo> findByTitleContainingIgnoreCase(String title);
+
+
 }
