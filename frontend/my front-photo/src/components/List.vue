576c10adf2d5346db9054c0c824587d0adfa616b
Daniel <angelicodaniel93@gmail.com>
2023-07-10 16:21:26 +0200

    front-end filter

--- frontend/my front-photo/src/components/List.vue
+++ frontend/my front-photo/src/components/List.vue
@@ -1,52 +1,69 @@
 <script>
 import FotoCard from "./FotoCard.vue";
 import axios from "axios";
-// import  {endpoint}  from "../stores/endpoint.js"
 
 export default {
   data() {
     return {
       photos: [],
-      isVisible:false,
-      title: "La mia lista di foto!"
+      isVisible: false,
+      title: "La mia lista di foto!",
+      filterText: ""
+    };
+  },
+
+  created() {
+    const endpoint = "http://localhost:8080/api/v1";
+    const getPhotos = async () => {
+      try {
+        const response = await axios.get(endpoint + "/photos");
+        this.photos = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPhotos();
+  },
+
+  computed: {
+    filteredPhotos() {
+      return this.photos.filter(photo => {
+        return photo.title.toLowerCase().includes(this.filterText.toLowerCase());
+      });
     }
-},
+  },
 
-created() {
-  const endpoint = "http://localhost:8080/api/v1";
-  const getPhotos = async () => {
-    try {
-      const response = await axios.get(endpoint + '/photos');
-      this.photos = response.data;
-
-    } catch (error) {
-      console.log(error);
+  methods: {
+    filterPhotos() {
+      // Esegui il filtraggio delle foto
+      // Utilizzando il computed property filteredPhotos
     }
-
-      console.log(this.photos);
-
-  };
-
-  getPhotos();
-},
-
+  },
 
   components: {
-    FotoCard,
-  },
+    FotoCard
+  }
 };
 </script>
-
 <template>
-  <div v-if="this.photos.lenght != 0" >
-    <h1 class="mt-3">{{ title }}</h1>
-    <div class="row mt-5">
-        <FotoCard v-for="photo in photos" :key="photo.id" :photo="photo"/>
-     </div>
+  <div class="titolo-ricerca row">
+    <div class="col-6">
+      <h1 class="mt-3">{{ title }}</h1>
+    </div>
+
+
+    <div class="col-6 d-flex flex-column justify-content-center">
+      <input type="text" v-model="filterText" placeholder="Filtra per titolo">
+      <!-- <button @click="filterPhotos">Filtra</button> -->
+    </div>
+
   </div>
-  <div v-else="" >Non ci sono foto al momento</div>
-
-
+  <div v-if="filteredPhotos.length !== 0">
+    <div class="row mt-5">
+      <FotoCard v-for="photo in filteredPhotos" :key="photo.id" :photo="photo"/>
+    </div>
+  </div>
+  <h1 class="fw-bold mt-5" v-else>Al momento non ci sono foto</h1>
 </template>
-
 <style lang="scss" scoped></style>
