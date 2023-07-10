b8cd35aef2bf59a2d303b9c520f9ce9d3108627c
Daniel <angelicodaniel93@gmail.com>
2023-07-10 12:59:00 +0200

    frontend

---
+++ frontend/frontend/src/components/FotoCard.vue
@@ -0,0 +1,41 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      
+    }
+  }
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <div class="col-4 col-md-6">
+
+    <div class="card mb-3 text-light">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="https://i1.sndcdn.com/artworks-JgZTXlfHGzsoG7BZ-r6esOA-t500x500.jpg" class="img-fluid rounded-start" alt="photo">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Photo card</h5>
+          <p class="card-text">Photo description</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+
+          <button class="btn btn-light">button</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+</template>
+<style lang="scss" scoped></style>
+
+
